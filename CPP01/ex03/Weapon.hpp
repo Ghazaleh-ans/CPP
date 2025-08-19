67b6ec1bca3af6e11be1d5bd5b1dffeabad3ac5d
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2025-08-19 23:46:13 +0200

    ex03 is done

---
+++ CPP01/ex03/Weapon.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/19 20:35:48 by gansari           #+#    #+#             */
+/*   Updated: 2025/08/19 20:37:42 by gansari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+#include <string>
+#include <iostream>
+
+class Weapon {
+private:
+	std::string type;
+public:
+	Weapon(const std::string &type);
+	~Weapon();
+	const std::string &getType() const;
+	void setType(const std::string &newType);
+};
+
+#endif
---
+++ CPP01/ex03/main.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/19 20:42:16 by gansari           #+#    #+#             */
+/*   Updated: 2025/08/19 23:44:34 by gansari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+/*
+Use References when(HumanA):
+- Object always needs the referenced item
+- The relationship won't change during the object's lifetime
+- You want simpler, safer syntax
+
+Use Pointers when(HumanB):
+- Object may or may not have the item (optional relationship)
+- You need to reassign to different objects
+- You need to represent "no object" (NULL)
+*/
+
+int main() {
+	Weapon club = Weapon("crude spiked club");
+	Weapon sword = Weapon("sharp sword");
+
+	HumanA bob("Bob", club);
+	bob.attack();
+
+	club.setType("some other type of club");
+	bob.attack();
+
+	HumanB jim("Jim");
+	jim.setWeapon(club);
+	jim.attack();
+
+	HumanB jane("Jane");
+	jane.attack();
+	jane.setWeapon(club);
+	jane.attack();
+	club.setType("another type of club");
+	jane.attack();
+	jane.setWeapon(sword);
+	jane.attack();
+
+	return 0;
+}
