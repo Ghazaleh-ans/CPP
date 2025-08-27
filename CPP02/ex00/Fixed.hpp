aa6d0c588ec89510423c9a2015369efe412280b9
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2025-08-27 17:04:47 +0200

    ex00 is done

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 15:55:41 by gansari           #+#    #+#             */
+/*   Updated: 2025/08/27 16:12:05 by gansari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+private:
+	int					value;
+	static const int	bits = 8;
+public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+};
