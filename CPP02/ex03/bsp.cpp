fccc76e0379e45fb3ce328be7d72395123ada873
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2025-09-08 23:49:31 +0200

    ex03 is done

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 22:33:50 by gansari           #+#    #+#             */
-/*   Updated: 2025/09/08 22:43:48 by gansari          ###   ########.fr       */
+/*   Updated: 2025/09/08 22:55:35 by gansari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 static Fixed area(Point const a, Point const b, Point const c)
 {
 	Fixed area = ((a.getX() * (b.getY() - c.getY())) +
-				  (b.getX() * (c.getY() - a.getY())) +
-				  (c.getX() * (a.getY() - b.getY()))).abs();
+				(b.getX() * (c.getY() - a.getY())) +
+				(c.getX() * (a.getY() - b.getY()))).abs();
 	return area / Fixed(2);
+}
+
+bool isEqual(const Fixed& a, const Fixed& b, const Fixed& epsilon)
+{
+	Fixed diff = (a - b).abs();
+	return diff <= epsilon;
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
@@ -27,7 +33,11 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	Fixed A2 = area(a, point, c);
 	Fixed A3 = area(a, b, point);
 
-	return (A == A1 + A2 + A3 && A1 != Fixed(0) && A2 != Fixed(0) && A3 != Fixed(0));
+	Fixed epsilon(0.001f);
+	Fixed sum = A1 + A2 + A3;
+
+	return (isEqual(A, sum, epsilon) && 
+			A1 > epsilon && A2 > epsilon && A3 > epsilon);
 }
 
 std::ostream &operator<<(std::ostream &out, const Point &point)
