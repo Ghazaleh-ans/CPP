3f6a56c6bbee307a9be801714fdcaf1f8dc41191
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2025-09-10 21:34:59 +0200

    first commit cpp04

---
+++ CPP04/ex00/Animal.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 21:27:45 by gansari           #+#    #+#             */
+/*   Updated: 2025/09/10 21:34:14 by gansari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+#include <iostream>
+#include <string>
+
+class Animal
+{
+protected:
+	std::string type;
+public:
+	Animal();
+	Animal(const std::string &type);
+	Animal &operator=(const Animal &other);
+	~Animal();
+	void setType(std::string type);
+	void getType() const;
+};
+
+#endif
