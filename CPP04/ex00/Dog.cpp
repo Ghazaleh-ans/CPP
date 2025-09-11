ac307970bad6c5a5f803b11e835755b1139ae252
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2025-09-11 10:38:46 +0200

    WrongAnimal and WrongCat is added, Animal, Cat and Dog Copy Constructors are added

--- CPP04/ex00/Dog.cpp
+++ CPP04/ex00/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 23:20:00 by gansari           #+#    #+#             */
-/*   Updated: 2025/09/10 23:20:00 by gansari          ###   ########.fr       */
+/*   Updated: 2025/09/11 10:31:50 by gansari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 Dog::Dog() : Animal("Dog")
 {
-	std::cout << "Dog " << type << " has been created with default constructor." << std::endl;
+	std::cout << "Dog default constructor called" << std::endl;
 }
 
-Dog::Dog(const std::string &type) : Animal("Dog")
+Dog::Dog(const Dog &other) : Animal(other)
 {
-	std::cout << "Dog " << type << " has been created with parameterized constructor." << std::endl;
+	std::cout << "Dog copy constructor called" << std::endl;
 }
 
 Dog &Dog::operator=(const Dog &other)
@@ -28,16 +28,16 @@ Dog &Dog::operator=(const Dog &other)
 	{
 		Animal::operator=(other);
 	}
-	std::cout << "Dog assignment operator called for " << type << std::endl;
+	std::cout << "Dog assignment operator called" << std::endl;
 	return *this;
 }
 
 Dog::~Dog()
 {
-	std::cout << "Dog " << type << " has been destroyed." << std::endl;
+	std::cout << "Dog destructor called" << std::endl;
 }
 
 void Dog::makeSound() const
 {
-	std::cout << "Dog says Woof Woof" << std::endl;
+	std::cout << "Woof! Woof!" << std::endl;
 }
--- CPP04/ex00/WrongAnimal.hpp
+++ CPP04/ex00/WrongAnimal.hpp
@@ -1,34 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/10 21:27:45 by gansari           #+#    #+#             */
-/*   Updated: 2025/09/11 10:09:46 by gansari          ###   ########.fr       */
+/*   Created: 2025/09/11 10:21:12 by gansari           #+#    #+#             */
+/*   Updated: 2025/09/11 10:21:14 by gansari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ANIMAL_HPP
-#define ANIMAL_HPP
+#ifndef WRONGANIMAL_HPP
+#define WRONGANIMAL_HPP
 #include <iostream>
 #include <string>
 
-class Animal
+class WrongAnimal
 {
 protected:
 	std::string type;
 
 public:
-	Animal();
-	Animal(const std::string &type);
-	Animal(const Animal &other);
-	Animal &operator=(const Animal &other);
-	virtual ~Animal();
-	virtual void makeSound() const;
-	virtual void setType(const std::string &type);
-	virtual std::string getType() const;
+	WrongAnimal();
+	WrongAnimal(const std::string &type);
+	WrongAnimal(const WrongAnimal &other);
+	WrongAnimal &operator=(const WrongAnimal &other);
+	virtual ~WrongAnimal();
+	void makeSound() const;  // Notice: NOT virtual - this is the key difference
+	void setType(const std::string &type);
+	std::string getType() const;
 };
 
 #endif
