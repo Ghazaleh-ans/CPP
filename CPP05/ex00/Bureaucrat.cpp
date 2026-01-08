57f556fe53652b6a06651205b90766cd1784a77f
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2026-01-08 18:34:41 +0100

    Bureaucrat class is done

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -1,9 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 18:29:51 by gansari           #+#    #+#             */
+/*   Updated: 2026/01/08 18:32:33 by gansari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Bureaucrat.hpp"
 
-// Default constructor
 Bureaucrat::Bureaucrat() : _name("Default"), _grade(150) {}
 
-// Parameterized constructor
 Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name) {
 	if (grade < 1)
 		throw GradeTooHighException();
@@ -12,22 +22,17 @@ Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name) {
 	_grade = grade;
 }
 
-// Copy constructor
 Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade) {}
 
-// Assignment operator (note: can't change const _name, only _grade)
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
 	if (this != &other) {
 		_grade = other._grade;
-		// _name is const, cannot be assigned
 	}
 	return *this;
 }
 
-// Destructor
 Bureaucrat::~Bureaucrat() {}
 
-// Getters
 const std::string& Bureaucrat::getName() const {
 	return _name;
 }
@@ -36,21 +41,18 @@ int Bureaucrat::getGrade() const {
 	return _grade;
 }
 
-// Increment = better grade (decrease number)
 void Bureaucrat::incrementGrade() {
 	if (_grade <= 1)
 		throw GradeTooHighException();
 	_grade--;
 }
 
-// Decrement = worse grade (increase number)
 void Bureaucrat::decrementGrade() {
 	if (_grade >= 150)
 		throw GradeTooLowException();
 	_grade++;
 }
 
-// Exception implementations
 const char* Bureaucrat::GradeTooHighException::what() const throw() {
 	return "Grade is too high!";
 }
@@ -59,7 +61,6 @@ const char* Bureaucrat::GradeTooLowException::what() const throw() {
 	return "Grade is too low!";
 }
 
-// Insertion operator overload
 std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat) {
 	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
 	return out;
