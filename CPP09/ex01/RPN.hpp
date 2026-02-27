7a69c5b82a0c0b243801994561489c7a91187bb1
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2026-02-27 15:11:08 +0100

    RPN is created

---
+++ CPP09/ex01/RPN.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/27 14:53:29 by gansari           #+#    #+#             */
+/*   Updated: 2026/02/27 14:54:29 by gansari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <stack>
+# include <string>
+
+class RPN
+{
+public:
+	RPN();
+	RPN(const RPN& other);
+	RPN& operator=(const RPN& other);
+	~RPN();
+
+	int evaluate(const std::string& expression) const;
+
+private:
+	static int applyOperator(char op, int left, int right);
+};
+
+#endif
