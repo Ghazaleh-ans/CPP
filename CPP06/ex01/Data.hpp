eb677f7a0e5caf320e26db105067b5dd6cc5cb5d
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2026-04-20 17:41:17 +0200

    ex01 is done

--- CPP06/ex01/Data.hpp
+++ CPP06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 12:37:22 by gansari           #+#    #+#             */
-/*   Updated: 2026/02/11 14:37:49 by gansari          ###   ########.fr       */
+/*   Updated: 2026/04/20 17:07:10 by gansari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@
 #include <string>
 
 struct Data {
-	// Member variables
 	std::string	name;
 	int			age;
 	bool		hasPet;
 	std::string	petName;
-	// Constructors for easy initialization
+
 	Data();
 	Data(const std::string& n, int a, bool pet, const std::string& pName);
 };
